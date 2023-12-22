787effda11ee0bc86763298cad51bbd9bcfe68d0
Andrey Ulanov <andy.ulanov@gmail.com>
2023-12-22 11:35:11 +0400

    Initial Commit

---
+++ CoffeeHouses-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeHouses-iOS
+//
+//  Created by Andrey Ulanov on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
