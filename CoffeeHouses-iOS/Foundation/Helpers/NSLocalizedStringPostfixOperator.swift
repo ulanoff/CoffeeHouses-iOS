47193bfefe665a276511a681823ef41f7bf3b12b
Andrey Ulanov <andy.ulanov@gmail.com>
2023-12-23 20:25:26 +0400

    add Localizable.strings files and localization helper

---
+++ CoffeeHouses-iOS/Foundation/Helpers/NSLocalizedStringPostfixOperator.swift
@@ -0,0 +1,13 @@
+//
+//  NSLocalizedStringPostfixOperator.swift
+//  CoffeeHouses-iOS
+//
+//  Created by Andrey Ulanov on 23.12.2023.
+//
+
+import Foundation
+
+postfix operator <
+postfix func < (string: String) -> String {
+    return NSLocalizedString(string, comment: "")
+}
