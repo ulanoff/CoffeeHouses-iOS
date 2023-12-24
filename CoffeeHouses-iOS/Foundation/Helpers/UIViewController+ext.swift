2d4ec9f77d990b48906a6298e4125ce6d4583ede
Andrey Ulanov <andy.ulanov@gmail.com>
2023-12-24 17:13:29 +0400

    implement UIViewController helper for keyboard hiding

---
+++ CoffeeHouses-iOS/Foundation/Helpers/UIViewController+ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+ext.swift
+//  CoffeeHouses-iOS
+//
+//  Created by Andrey Ulanov on 24.12.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
